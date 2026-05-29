a95d5f2fff0a3450b2bd58e1a0fa355ec37fabef
Liliya <78988408+lpanfilova@users.noreply.github.com>
2026-05-29 19:04:22 -0400

    test: cover server error response

--- client_test.go
+++ client_test.go
@@ -8,25 +8,41 @@ import (
 
 func TestGetWeather(t *testing.T) {
 	t.Run("returns weather when server responds 200 OK", func(t *testing.T) {
-		server := makeServer("sunny")
+		server := makeServer("sunny", http.StatusOK)
 
 		defer server.Close()
 
 		want := "sunny"
 		got, err := getWeather(server.URL)
 		if err != nil {
-			t.Fatalf("Failed to call server: %v", err)
+			t.Fatalf("failed to call server: %v", err)
 		}
 
 		if got != want {
 			t.Errorf("got %s, want %s", got, want)
 		}
 	})
+
+	t.Run("returns error when server responds 500", func(t *testing.T) {
+		server := makeServer("sunny", http.StatusInternalServerError)
+		defer server.Close()
+
+		want := ""
+		got, err := getWeather(server.URL)
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+
+		if err == nil {
+			t.Error("expected error, got nil")
+		}
+	})
 }
 
-func makeServer(weather string) *httptest.Server {
+func makeServer(body string, status int) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(weather))
+		w.WriteHeader(status)
+		w.Write([]byte(body))
 	}))
 }
