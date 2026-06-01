63825db0051d7ab7024e0bfe462df5cf2e36e76a
Liliya <78988408+lpanfilova@users.noreply.github.com>
2026-05-31 20:35:58 -0400

    feat: retry if server returns 429

--- client.go
+++ client.go
@@ -4,24 +4,48 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"strconv"
+	"time"
 )
 
-func getWeather(clientURL string) (string, error) {
+type Waiter interface {
+	Wait(time.Duration)
+}
+
+type RealWaiter struct{}
+
+func (s *RealWaiter) Wait(duration time.Duration) {
+	time.Sleep(duration)
+}
+
+func getWeather(clientURL string, w Waiter) (string, error) {
+	var weather string
+
 	resp, err := http.Get(clientURL)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode == 500 {
+	switch resp.StatusCode {
+	case 200:
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		weather = string(bodyBytes)
+
+	case 500:
 		return "", errors.New("Server is unavailable: status code 500")
+	case 429:
+		seconds, err := strconv.Atoi(resp.Header.Get("Retry-After"))
+		if err != nil {
+			return "", err
+		}
+		timeToWait := time.Duration(seconds) * time.Second
+		w.Wait(timeToWait)
+		weather, err = getWeather(clientURL, w)
 	}
 
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	bodyString := string(bodyBytes)
-
-	return bodyString, err
+	return weather, err
 }
--- client_test.go
+++ client_test.go
@@ -3,17 +3,28 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
+	"time"
 )
+
+type SpyWaiter struct {
+	durationWaited time.Duration
+}
+
+func (s *SpyWaiter) Wait(duration time.Duration) {
+	s.durationWaited = duration
+}
 
 func TestGetWeather(t *testing.T) {
 	t.Run("returns weather when server responds 200 OK", func(t *testing.T) {
+		waiter := &SpyWaiter{}
 		server := makeServer("sunny", http.StatusOK)
 
 		defer server.Close()
 
 		want := "sunny"
-		got, err := getWeather(server.URL)
+		got, err := getWeather(server.URL, waiter)
 		if err != nil {
 			t.Fatalf("failed to call server: %v", err)
 		}
@@ -24,11 +35,12 @@ func TestGetWeather(t *testing.T) {
 	})
 
 	t.Run("returns error when server responds 500", func(t *testing.T) {
+		waiter := &SpyWaiter{}
 		server := makeServer("sunny", http.StatusInternalServerError)
 		defer server.Close()
 
 		want := ""
-		got, err := getWeather(server.URL)
+		got, err := getWeather(server.URL, waiter)
 
 		if got != want {
 			t.Errorf("got %s, want %s", got, want)
@@ -38,11 +50,50 @@ func TestGetWeather(t *testing.T) {
 			t.Error("expected error, got nil")
 		}
 	})
+
+	t.Run("retries after 'Retry-After' seconds if a weather server returns 429", func(t *testing.T) {
+		timeToWait := 2
+		server := makeRetryServer("sunny", timeToWait)
+		defer server.Close()
+
+		waiter := &SpyWaiter{}
+
+		want := "sunny"
+		got, err := getWeather(server.URL, waiter)
+		if err != nil {
+			t.Fatalf("server failed to retry: %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+
+		if waiter.durationWaited != time.Duration(timeToWait)*time.Second {
+			t.Errorf("waited %v seconds, want to wait %v seconds", waiter.durationWaited, 1)
+		}
+	})
 }
 
 func makeServer(body string, status int) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
+		w.Write([]byte(body))
+	}))
+}
+
+func makeRetryServer(body string, retrySec int) *httptest.Server {
+	requestCount := 0
+
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestCount++
+
+		if requestCount == 1 {
+			w.Header().Set("Retry-After", strconv.Itoa(retrySec))
+			w.WriteHeader(http.StatusTooManyRequests)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(body))
 	}))
 }
